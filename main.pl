037267587f49d27340d7d27b00ce6b4f1130c52d
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-11-17 15:30:41 -0300

    Atualizacao da Main (prolog)

--- main.pl
+++ main.pl
@@ -241,15 +241,22 @@ show_mem(_,0) :- energia(E), pontuacao(P), write('E: '), write(E), write('   P: 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Regras jogo
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %apagar esta linha - apenas para demonstracao aleatoria
-executa_acao(X) :- L=['virar_esquerda','virar_direita','andar','pegar'],random_between(1,4,I), nth1(I, L, X),!.
+%executa_acao(X) :- L=['virar_esquerda','virar_direita','andar','pegar'],random_between(1,4,I), nth1(I, L, X),!.
 
 %apagar linhas abaixo... sao exemplos de resposta
 %executa_acao(andar) :- posicao(PX, _, oeste), PX > 1, X = andar,!.
 %executa_acao(andar) :- posicao(PX, _, leste), PX < 3, X = andar,!.
 %executa_acao(pegar) :- posicao(PX, PY,_), tem_ouro(PX, PY), !.
 %executa_acao(voltar) :- peguei_todos_ouros,!.
+
+executa_acao(pegar) :- posicao(PX, PY, _), tem_ouro(PX, PY), !.
+executa_acao(pegar) :- posicao(PX, PY, _), tem_pocao(PX, PY), energia(E) <= 50, !.
+
+
 
 
 
