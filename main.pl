fc176dde18207f04176f6337f3a6eace68543010
fbenevolo <benevolofelipe@gmail.com>
2025-11-27 18:08:55 -0300

    main.pl restored

---
+++ main.pl
@@ -0,0 +1,407 @@
+
+:-dynamic posicao/3.
+:-dynamic memory/3.
+:-dynamic visitado/2.
+:-dynamic certeza/2.
+:-dynamic energia/1.
+:-dynamic pontuacao/1.
+:-dynamic qtd_ouros/1.
+
+:-consult('mapa-facil.pl').
+
+delete([], _, []).
+delete([Elem|Tail], Del, Result) :-
+    (   \+ Elem \= Del
+    ->  delete(Tail, Del, Result)
+    ;   Result = [Elem|Rest],
+        delete(Tail, Del, Rest)
+    ).
+	
+
+
+reset_game :- retractall(memory(_,_,_)), 
+			retractall(visitado(_,_)), 
+			retractall(certeza(_,_)),
+			retractall(energia(_)),
+			retractall(pontuacao(_)),
+			retractall(posicao(_,_,_)),
+			assert(energia(100)),
+			assert(pontuacao(0)),
+			assert(posicao(1,1, norte)),
+			assert(qtd_ouros(0)).
+
+
+:-reset_game.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Controle de Status
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%atualiza pontuacao
+atualiza_pontuacao(X):- pontuacao(P), retract(pontuacao(P)), NP is P + X, assert(pontuacao(NP)),!.
+
+%atualiza energia
+atualiza_energia(N):- energia(E), retract(energia(E)), NE is E + N, 
+					(
+					 (NE =<0, assert(energia(0)),posicao(X,Y,_),retract(posicao(_,_,_)), assert(posicao(X,Y,morto)),!);
+					 (NE >100, assert(energia(100)),!);
+					  (NE >0,assert(energia(NE)),!)
+					 ).
+
+%verifica situacao da nova posicao e atualiza energia e pontos
+verifica_player :- posicao(X,Y,_), tile(X,Y,'P'), atualiza_energia(-100), atualiza_pontuacao(-1000),!.
+verifica_player :- posicao(X,Y,_), tile(X,Y,'D'), random_between(-80,-50,D), atualiza_energia(D),!.
+verifica_player :- posicao(X,Y,_), tile(X,Y,'d'), random_between(-50,-25,D), atualiza_energia(D),!.
+verifica_player :- posicao(X,Y,Z), tile(X,Y,'T'), 
+					map_size(SX,SY), random_between(1,SX,NX), random_between(1,SY,NY),
+				retract(posicao(X,Y,Z)), assert(posicao(NX,NY,Z)), atualiza_obs, verifica_player,!.
+verifica_player :- true.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Comandos
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%virar direita
+virar_direita :- posicao(X,Y, norte), retract(posicao(_,_,_)), assert(posicao(X, Y, leste)),atualiza_pontuacao(-1),!.
+virar_direita :- posicao(X,Y, oeste), retract(posicao(_,_,_)), assert(posicao(X, Y, norte)),atualiza_pontuacao(-1),!.
+virar_direita :- posicao(X,Y, sul), retract(posicao(_,_,_)), assert(posicao(X, Y, oeste)),atualiza_pontuacao(-1),!.
+virar_direita :- posicao(X,Y, leste), retract(posicao(_,_,_)), assert(posicao(X, Y, sul)),atualiza_pontuacao(-1),!.
+
+%virar esquerda
+virar_esquerda :- posicao(X,Y, norte), retract(posicao(_,_,_)), assert(posicao(X, Y, oeste)),atualiza_pontuacao(-1),!.
+virar_esquerda :- posicao(X,Y, oeste), retract(posicao(_,_,_)), assert(posicao(X, Y, sul)),atualiza_pontuacao(-1),!.
+virar_esquerda :- posicao(X,Y, sul), retract(posicao(_,_,_)), assert(posicao(X, Y, leste)),atualiza_pontuacao(-1),!.
+virar_esquerda :- posicao(X,Y, leste), retract(posicao(_,_,_)), assert(posicao(X, Y, norte)),atualiza_pontuacao(-1),!.
+
+%andar
+andar :- posicao(X,Y,P), P = norte, map_size(_,MAX_Y), Y < MAX_Y, YY is Y + 1, 
+         retract(posicao(X,Y,_)), assert(posicao(X, YY, P)), 
+		 %((retract(certeza(X,YY)), assert(certeza(X,YY))); assert(certeza(X,YY))),
+		 set_real(X,YY),
+		 ((retract(visitado(X,Y)), assert(visitado(X,Y))); assert(visitado(X,Y))),atualiza_pontuacao(-1),!.
+		 
+andar :- posicao(X,Y,P), P = sul,  Y > 1, YY is Y - 1, 
+         retract(posicao(X,Y,_)), assert(posicao(X, YY, P)), 
+		 %((retract(certeza(X,YY)), assert(certeza(X,YY))); assert(certeza(X,YY))),
+		 set_real(X,YY),
+		 ((retract(visitado(X,Y)), assert(visitado(X,Y))); assert(visitado(X,Y))),atualiza_pontuacao(-1),!.
+
+andar :- posicao(X,Y,P), P = leste, map_size(MAX_X,_), X < MAX_X, XX is X + 1, 
+         retract(posicao(X,Y,_)), assert(posicao(XX, Y, P)), 
+		 %((retract(certeza(XX,Y)), assert(certeza(XX,Y))); assert(certeza(XX,Y))),
+		 set_real(XX,Y),
+		 ((retract(visitado(X,Y)), assert(visitado(X,Y))); assert(visitado(X,Y))),atualiza_pontuacao(-1),!.
+
+andar :- posicao(X,Y,P), P = oeste,  X > 1, XX is X - 1, 
+         retract(posicao(X,Y,_)), assert(posicao(XX, Y, P)), 
+		 %((retract(certeza(XX,Y)), assert(certeza(XX,Y))); assert(certeza(XX,Y))),
+		 set_real(XX,Y),
+		 ((retract(visitado(X,Y)), assert(visitado(X,Y))); assert(visitado(X,Y))),atualiza_pontuacao(-1),!.
+		 
+%pegar	
+pegar :- posicao(X,Y,_), tile(X,Y,'O'), retract(tile(X,Y,'O')), assert(tile(X,Y,'')), atualiza_pontuacao(-5), atualiza_pontuacao(500),set_real(X,Y),!. 
+pegar :- posicao(X,Y,_), tile(X,Y,'U'), retract(tile(X,Y,'U')), assert(tile(X,Y,'')), atualiza_pontuacao(-5), atualiza_energia(50),set_real(X,Y),!. 
+pegar :- atualiza_pontuacao(-5),!.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Funcoes Auxiliares de navegação e observação
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		 
+%Define as 4 adjacencias		 
+adjacente(X, Y) :- posicao(PX, Y, _), map_size(MAX_X,_),PX < MAX_X, X is PX + 1.  
+adjacente(X, Y) :- posicao(PX, Y, _), PX > 1, X is PX - 1.  
+adjacente(X, Y) :- posicao(X, PY, _), map_size(_,MAX_Y),PY < MAX_Y, Y is PY + 1.  
+adjacente(X, Y) :- posicao(X, PY, _), PY > 1, Y is PY - 1.  
+
+%cria lista com a adjacencias
+adjacentes(L) :- findall(Z,(adjacente(X,Y),tile(X,Y,Z)),L).
+
+%define observacoes locais
+observacao_loc(brilho,L) :- member('O',L).
+observacao_loc(reflexo,L) :- member('U',L).
+
+%define observacoes adjacentes
+observacao_adj(brisa,L) :- member('P',L).
+observacao_adj(palmas,L) :- member('T',L).
+observacao_adj(passos,L) :- member('D',L).
+observacao_adj(passos,L) :- member('d',L).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Tratamento de KB e observações
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%consulta e processa observações
+atualiza_obs:-adj_cand_obs(LP), observacoes(LO), iter_pos_list(LP,LO), observacao_certeza, observacao_vazia.
+
+%adjacencias candidatas p/ a observacao (aquelas não visitadas)
+adj_cand_obs(L) :- findall((X,Y), (adjacente(X, Y), \+visitado(X,Y)), L).
+
+%cria lista de observacoes
+observacoes(X) :- adjacentes(L), findall(Y, observacao_adj(Y,L), X).
+
+%itera posicoes da lista para adicionar observacoes
+iter_pos_list([], _) :- !.
+iter_pos_list([H|T], LO) :- H=(X,Y), 
+							((corrige_observacoes_antigas(X, Y, LO),!);
+							adiciona_observacoes(X, Y, LO)),
+							iter_pos_list(T, LO).							 
+
+%Corrige observacoes antigas na memoria que ficaram com apenas uma adjacencia
+corrige_observacoes_antigas(X, Y, []):- \+certeza(X,Y), memory(X,Y,[]).
+corrige_observacoes_antigas(X, Y, LO):-
+	\+certeza(X,Y), \+ memory(X,Y,[]), memory(X, Y, LM), intersection(LO, LM, L), 
+	retract(memory(X, Y, LM)), assert(memory(X, Y, L)).
+
+%Adiciona observacoes na memoria
+adiciona_observacoes(X, Y, _) :- certeza(X,Y),!.
+adiciona_observacoes(X, Y, LO) :- \+certeza(X,Y), \+ memory(X,Y,_), assert(memory(X, Y, LO)).
+
+%Quando há apenas uma observação e uma unica posição incerta, deduz que a observação está na casa incerta
+%e marca como certeza
+%observacao_certeza:- findall((X,Y), (adjacente(X, Y), 
+%						((\+visitado(X,Y), \+certeza(X,Y));(certeza(X,Y),memory(X,Y,ZZ),ZZ\=[])),
+%						memory(X,Y,Z), Z\=[]), L), ((length(L,1),L=[(XX,YY)], assert(certeza(XX,YY)),!);true).
+						
+observacao_certeza:- observacao_certeza('brisa'),
+						observacao_certeza('palmas'),
+						observacao_certeza('passos').
+						
+observacao_certeza(Z):- findall((X,Y), (adjacente(X, Y), 
+						((\+visitado(X,Y), \+certeza(X,Y));(certeza(X,Y),memory(X,Y,[Z]))),
+						memory(X,Y,[Z])), L), ((length(L,1),L=[(XX,YY)], assert(certeza(XX,YY)),!);true).						
+
+%Quando posição não tem observações
+observacao_vazia:- adj_cand_obs(LP), observacao_vazia(LP).
+observacao_vazia([]) :- !.
+observacao_vazia([H|T]) :- H=(X,Y), ((memory(X,Y,[]), \+certeza(X,Y),assert(certeza(X,Y)),!);true), observacao_vazia(T).
+
+%Quando posicao é visitada, atualiza memoria de posicao com a informação real do mapa 
+set_real(X,Y):- ((retract(certeza(X,Y)), assert(certeza(X,Y)),!); assert(certeza(X,Y))), set_real2(X,Y),!.
+set_real2(X,Y):- tile(X,Y,'P'), ((retract(memory(X,Y,_)),assert(memory(X,Y,[brisa])),!);assert(memory(X,Y,[brisa]))),!.
+set_real2(X,Y):- tile(X,Y,'O'), ((retract(memory(X,Y,_)),assert(memory(X,Y,[brilho])),!);assert(memory(X,Y,[brilho]))),!.
+set_real2(X,Y):- tile(X,Y,'T'), ((retract(memory(X,Y,_)),assert(memory(X,Y,[palmas])),!);assert(memory(X,Y,[palmas]))),!.
+set_real2(X,Y):- ((tile(X,Y,'D'),!); tile(X,Y,'d')), ((retract(memory(X,Y,_)),assert(memory(X,Y,[passos])),!);assert(memory(X,Y,[passos]))),!.
+set_real2(X,Y):- tile(X,Y,'U'), ((retract(memory(X,Y,_)),assert(memory(X,Y,[reflexo])),!);assert(memory(X,Y,[reflexo]))),!.
+set_real2(X,Y):- tile(X,Y,''), ((retract(memory(X,Y,_)),assert(memory(X,Y,[])),!);assert(memory(X,Y,[]))),!.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Mostra mapa real
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+show_player(X,Y) :- posicao(X,Y, norte), write('^'),!.
+show_player(X,Y) :- posicao(X,Y, oeste), write('<'),!.
+show_player(X,Y) :- posicao(X,Y, leste), write('>'),!.
+show_player(X,Y) :- posicao(X,Y, sul), write('v'),!.
+show_player(X,Y) :- posicao(X,Y, morto), write('+'),!.
+
+%show_position(X,Y) :- show_player(X,Y),!.
+show_position(X,Y) :- (show_player(X,Y); write(' ')), tile(X,Y,Z), ((Z='', write(' '));write(Z)),!.
+
+show_map :- map_size(_,MAX_Y), show_map(1,MAX_Y),!.
+show_map(X,Y) :- Y >= 1, map_size(MAX_X,_), X =< MAX_X, show_position(X,Y), write(' | '), XX is X + 1, show_map(XX, Y),!.
+show_map(X,Y) :- Y >= 1, map_size(X,_),YY is Y - 1, write(Y), nl, show_map(1, YY),!.
+show_map(_,0) :- energia(E), pontuacao(P), write('E: '), write(E), write('   P: '), write(P),!.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Mostra mapa conhecido
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+show_mem_info(X,Y) :- memory(X,Y,Z), 
+		((visitado(X,Y), write('.'),!); (\+certeza(X,Y), write('?'),!); (certeza(X,Y), write('!'))),
+		((member(brisa, Z), write('P'));write(' ')),
+		((member(palmas, Z), write('T'));write(' ')),
+		((member(brilho, Z), write('O'));write(' ')),
+		((member(passos, Z), write('D'));write(' ')),
+		((member(reflexo, Z), write('U'));write(' ')),!.
+
+show_mem_info(X,Y) :- \+memory(X,Y,[]), 
+			((visitado(X,Y), write('.'),!); (\+certeza(X,Y), write('?'),!); (certeza(X,Y), write('!'))),
+			write('     '),!.		
+		
+		
+
+show_mem_position(X,Y) :- posicao(X,Y,_), 
+		((visitado(X,Y), write('.'),!); (certeza(X,Y), write('!'),!); write(' ')),
+		write(' '), show_player(X,Y),
+		((memory(X,Y,Z),
+		((member(brilho, Z), write('O'));write(' ')),
+		((member(passos, Z), write('D'));write(' ')),
+		((member(reflexo, Z), write('U'));write(' ')),!);
+		(write('   '),!)).
+
+		
+show_mem_position(X,Y) :- show_mem_info(X,Y),!.
+
+
+show_mem :- map_size(_,MAX_Y), show_mem(1,MAX_Y),!.
+show_mem(X,Y) :- Y >= 1, map_size(MAX_X,_), X =< MAX_X, show_mem_position(X,Y), write('|'), XX is X + 1, show_mem(XX, Y),!.
+show_mem(X,Y) :- Y >= 1, map_size(X,_),YY is Y - 1, write(Y), nl, show_mem(1, YY),!.
+show_mem(_,0) :- energia(E), pontuacao(P), write('E: '), write(E), write('   P: '), write(P),!.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nao_seguro(X,Y) :- 
+    certeza(X,Y), 
+    memory(X, Y, L), 
+    (member(brisa, L) ; member(palmas, L) ; member(passos, L)).
+
+%Se já visitei, e sobrevivi, é seguro.
+seguro(X,Y) :- visitado(X,Y), !.
+
+% Se tenho certeza sobre ela e a memória não registra perigos mortais/teletransportes.
+seguro(X,Y) :- certeza(X,Y), memory(X, Y, L),
+				\+ member(brisa, L),    % não é poço
+    			\+ member(palmas, L),   % não é teletransporte
+    			\+ member(passos, L),   % não é monstro (Risco de dano)
+    			!.
+
+perigo_conhecido(X, Y, brisa) :- certeza(X,Y), memory(X, Y, L), member(brisa, L), !.
+perigo_conhecido(X, Y, passos) :- certeza(X,Y), memory(X, Y, L), member(passos, L), !.
+
+% Verifica se está dentro dos limites do mapa
+within_bounds(X, Y) :-
+    map_size(MaxX, MaxY),
+    X >= 1, X =< MaxX,
+    Y >= 1, Y =< MaxY.
+
+% Calcula coordenada à frente baseada na direção
+frente(X, Y, norte, X, NY) :- NY is Y + 1.
+frente(X, Y, sul, X, NY) :- NY is Y - 1.
+frente(X, Y, leste, NX, Y) :- NX is X + 1.
+frente(X, Y, oeste, NX, Y) :- NX is X - 1.
+
+% Verifica se existe alguma direção segura para explorar a partir de uma posição
+tem_direcao_segura(PX, PY) :-
+    frente(PX, PY, norte, NX, NY), 
+    within_bounds(NX, NY), 
+    seguro(NX, NY), 
+    \+ visitado(NX, NY).
+
+tem_direcao_segura(PX, PY) :-
+    frente(PX, PY, sul, NX, NY), 
+    within_bounds(NX, NY), 
+    seguro(NX, NY), 
+    \+ visitado(NX, NY).
+
+tem_direcao_segura(PX, PY) :-
+    frente(PX, PY, leste, NX, NY), 
+    within_bounds(NX, NY), 
+    seguro(NX, NY), 
+    \+ visitado(NX, NY).
+
+tem_direcao_segura(PX, PY) :-
+    frente(PX, PY, oeste, NX, NY), 
+    within_bounds(NX, NY), 
+    seguro(NX, NY), 
+    \+ visitado(NX, NY).
+
+% Calcula a direção para chegar na sala alvo
+calcular_direcao_para_sala(PX, PY, NX, NY, oeste) :- PX > NX, !.
+calcular_direcao_para_sala(PX, PY, NX, NY, leste) :- PX < NX, !.
+calcular_direcao_para_sala(PX, PY, NX, NY, sul) :- PY > NY, !.
+calcular_direcao_para_sala(PX, PY, NX, NY, norte) :- PY < NY, !.
+
+% Encontra uma sala visitada que tenha saídas não exploradas
+encontrar_sala_backtrack(PX, PY, NX, NY, Direcao) :-
+    visitado(NX, NY),
+    seguro(NX, NY),
+    tem_direcao_segura(NX, NY),  % Sala alvo tem saídas não exploradas
+    (NX \= PX ; NY \= PY),  % Não é a sala atual
+    calcular_direcao_para_sala(PX, PY, NX, NY, Direcao).
+
+% 1. EVITA APENAS POÇOS (morte certa)
+encontrar_sala_backtrack_evita_apenas_pocos(PX, PY, NX, NY, Direcao) :-
+    visitado(NX, NY),
+    seguro(NX, NY),
+    \+ perigo_conhecido(NX, NY, brisa),  % ← APENAS poços
+    (NX \= PX ; NY \= PY),
+    calcular_direcao_para_sala(PX, PY, NX, NY, Direcao).
+
+% 2. ACEITA MONSTROS se tiver energia suficiente
+encontrar_sala_backtrack_aceita_monstros(PX, PY, NX, NY, Direcao) :-
+    visitado(NX, NY),
+    seguro(NX, NY),
+    \+ perigo_conhecido(NX, NY, passos),  % Ainda evita poços
+    energia(E), E > 60,                  % Só aceita monstros se tiver energia
+    (NX \= PX ; NY \= PY),
+    calcular_direcao_para_sala(PX, PY, NX, NY, Direcao).
+
+% 3. ACEITA TUDO (último último recurso)
+encontrar_sala_backtrack_qualquer(PX, PY, NX, NY, Direcao) :-
+    visitado(NX, NY),
+    seguro(NX, NY),
+    (NX \= PX ; NY \= PY),       % Não é a sala atual
+    calcular_direcao_para_sala(PX, PY, NX, NY, Direcao).
+
+
+% Vira e/ou anda na direção da sala alvo
+direcionar_para_sala(DirecaoDesejada) :-
+    posicao(PX, PY, DirecaoAtual),
+    (DirecaoAtual == DirecaoDesejada -> 
+        andar  % Se já está na direção certa, anda
+    ;
+        virar_para_direcao(DirecaoDesejada)
+    ).
+
+% Vira até ficar na direção desejada (já existente)
+virar_para_direcao(DirecaoDesejada) :-
+    posicao(_, _, DirecaoAtual),
+    (DirecaoAtual == DirecaoDesejada -> true ;
+        (virar_direita, virar_para_direcao(DirecaoDesejada))
+    ), !.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Ações
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Acao pegar
+executa_acao(pegar) :- posicao(X, Y, _), memory(X, Y, L), member(brilho, L), !.
+executa_acao(pegar) :- posicao(X, Y, _), memory(X, Y, L), member(reflexo, L), energia(E), E <= 50, !.
+
+% Acao andar para novo e seguro
+executa_acao(andar) :- posicao(PX, PY, norte), map_size(_, MAX_Y), PY < MAX_Y, NX is PX, NY is PY + 1, seguro(NX, NY), \+ visitado(NX, NY), !.
+executa_acao(andar) :- posicao(PX, PY, sul), PY > 1, NX is PX, NY is PY - 1, seguro(NX, NY), \+ visitado(NX, NY), !.
+executa_acao(andar) :- posicao(PX, PY, leste), map_size(MAX_X, _), PX < MAX_X, NX is PX + 1, NY is PY, seguro(NX, NY), \+ visitado(NX, NY), !.
+executa_acao(andar) :- posicao(PX, PY, oeste), PX > 1, NX is PX - 1, NY is PY, seguro(NX, NY), \+ visitado(NX, NY), !.
+
+executa_acao(andar) :-
+    posicao(PX, PY, _),
+    \+ tem_direcao_segura(PX, PY),  % Preso na posição atual
+      \+ visitado(NX, NY),  % ← ADICIONE ESTA VERIFICAÇÃO
+    encontrar_sala_backtrack(PX, PY, NX, NY, Direcao),
+    direcionar_para_sala(Direcao),
+    !.
+
+executa_acao(andar) :-
+   posicao(PX, PY, _),
+   \+ tem_direcao_segura(PX, PY),  % Preso na posição atual
+   encontrar_sala_backtrack_evita_apenas_pocos(PX, PY, NX, NY, Direcao),
+   direcionar_para_sala(Direcao),
+   !.
+
+executa_acao(andar) :-
+   posicao(PX, PY, _),
+   \+ tem_direcao_segura(PX, PY),  % Preso na posição atual
+   encontrar_sala_backtrack_aceita_monstros(PX, PY, NX, NY, Direcao),
+   direcionar_para_sala(Direcao),
+   !.
+
+
+executa_acao(andar) :-
+    posicao(PX, PY, _),
+    \+ tem_direcao_segura(PX, PY), 
+    encontrar_sala_backtrack_qualquer(PX, PY, NX, NY, Direcao),
+    direcionar_para_sala(Direcao),
+    !.
+
+executa_acao(virar_direita) :-
+	borda_do_mapa;
+	(guarda_informacao_a_frente(Obs), apresenta_perigo(Obs));
+	(
+		adjacente_seguro_e_nao_visitado,
+		frente(X, Y), visitado(X, Y)
+	).
+executa_acao(andar).
